7c8b580b457a18f6b39dc05ef66827173c003f5e
Nureev.I <nureev.i@selectel.ru>
2023-10-23 17:11:51 +0300

    fc

--- glabels/__init__.py
+++ glabels/__init__.py
@@ -1,3 +1,3 @@
-from glabels._base import GlabelsBatchRunError
-from glabels.glabels_qt import GLabelsBatchQT
-from glabels.glabels_3 import Glabels3Batch
+from glabels._base import GlabelsBatchRunError  # noqa: F401
+from glabels.glabels_qt import GLabelsBatchQT  # noqa: F401
+from glabels.glabels_3 import Glabels3Batch  # noqa: F401
